7f4fc6e9eb878dadb8afbb54acda8001964830d2
anivlam35 <danilovivan35@gmail.com>
2026-02-07 19:03:23 +0200

    Add RTD processing tools and tutorial

---
+++ my_RTD_processing/setup.sh
@@ -0,0 +1,23 @@
+# Set the path to development version of the data commissioning software:
+export SW_DEV_BASE_DIR="/sps/nemo/scratch/mauger/sw"
+
+# Load Yves' Linuxbrew installation:
+if [ -f ${SW_DEV_BASE_DIR}/SuperNEMO/config/linuxbrew_pro.bash ]; then
+   source ${SW_DEV_BASE_DIR}/SuperNEMO/config/linuxbrew_pro.bash
+fi
+# Load Francois's Bayeux library installation:
+if [ -f ${SW_DEV_BASE_DIR}/SuperNEMO/config/bayeux.bash ]; then
+   source ${SW_DEV_BASE_DIR}/SuperNEMO/config/bayeux.bash
+fi
+
+
+# Load Francois's SNCABLING library installation:
+if [ -f ${SW_DEV_BASE_DIR}/SuperNEMO/config/sncabling.bash ]; then
+   source ${SW_DEV_BASE_DIR}/SuperNEMO/config/sncabling.bash
+fi
+
+
+# Load Francois's SNFEE library installation:
+if [ -f ${SW_DEV_BASE_DIR}/SuperNEMO/config/snfee.bash ]; then
+   source ${SW_DEV_BASE_DIR}/SuperNEMO/config/snfee.bash
+fi
